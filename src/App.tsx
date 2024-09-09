836c8424a49a82a6c29f00fe50f04ea8af1e97db
Hung Bui <hungbui7690@gmail.com>
2024-09-09 05:35:20 -0600

    create single page for /users and /products

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Users from './pages/users/Users'
+import User from './pages/user/User'
 import Products from './pages/products/Products'
+import Product from './pages/product/Product'
 import SharedLayout from './pages/SharedLayout'
 
 const router = createBrowserRouter([
@@ -20,6 +22,14 @@ const router = createBrowserRouter([
       {
         path: 'products',
         element: <Products />,
+      },
+      {
+        path: 'users/:id',
+        element: <User />,
+      },
+      {
+        path: '/products/:id',
+        element: <Product />,
       },
     ],
   },
